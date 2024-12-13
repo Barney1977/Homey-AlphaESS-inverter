8a2d55f8b1668542a839ac38f58b838ef49d59d8
Markus Kling <markus.kling@gmail.com>
2024-12-13 18:00:11 +0000

    Corrected factor for PV

--- modbus/reader.js
+++ modbus/reader.js
@@ -35,68 +35,66 @@ class ModbusReader {
       socket.on('connect', async () => {
         const result = {};
 
-        for (const reg of registers) {
-          // eslint-disable-next-line radix
-          const res = await client.readHoldingRegisters(parseInt(reg.register), reg.len);
-          let val = null;
+        try {
+          for (const reg of registers) {
+            // eslint-disable-next-line radix
+            const res = await client.readHoldingRegisters(parseInt(reg.register), reg.len);
+            let val = null;
 
-          // eslint-disable-next-line default-case
-          switch (reg.type) {
-            case 'string':
-              val = res.response.body.valuesAsBuffer.toString().replace(/\0/ig, '');
-              break;
+            switch (reg.type) {
+              case 'string':
+                val = res.response.body.valuesAsBuffer.toString().replace(/\0/ig, '');
+                break;
 
-            case 'uint16be':
-              val = res.response.body.valuesAsBuffer.readUint16BE();
-              break;
+              case 'uint16be':
+                val = res.response.body.valuesAsBuffer.readUint16BE();
+                break;
 
-            case 'int16be':
-              val = res.response.body.valuesAsBuffer.readInt16BE();
-              break;
+              case 'int16be':
+                val = res.response.body.valuesAsBuffer.readInt16BE();
+                break;
 
-            case 'uint32be':
-              val = res.response.body.valuesAsBuffer.readUint32BE();
-              break;
+              case 'uint32be':
+                val = res.response.body.valuesAsBuffer.readUint32BE();
+                break;
 
-            case 'int32be':
-              val = res.response.body.valuesAsBuffer.readInt32BE();
-              break;
+              case 'int32be':
+                val = res.response.body.valuesAsBuffer.readInt32BE();
+                break;
 
-            default:
-              reject(new Error('Unkonw type ' || reg.type));
-          }
+              default:
+                reject(new Error('Unkonw type ' || reg.type));
+            }
 
-          result[reg.register] = {
-            name: reg.name_en,
-            unit: reg.unit,
-          };
+            result[reg.register] = {
+              name: reg.name_en,
+              unit: reg.unit,
+            };
 
-          if (reg.type === 'string') {
-            result[reg.register].value = val;
-          }
-          if (reg.frontend_type === 'bits') {
-            const bitstr = dec2bin(val).padStart(reg.len * 16, '0');
+            if (reg.type === 'string') {
+              result[reg.register].value = val;
+            }
 
-            result[reg.register].value = bitstr;
-            result[reg.register].value_string = reg.bits.map((e, i) => {
-              return bitstr[bitstr.length - i - 1] === '1' ? e : null;
-            }).filter((e) => e).join(',');
-          } else {
-            // eslint-disable-next-line no-lonely-if
-            if (reg.frontend_type === 'enum') {
+            if (reg.frontend_type === 'bits') {
+              const bitstr = dec2bin(val).padStart(reg.len * 16, '0');
+
+              result[reg.register].value = bitstr;
+              result[reg.register].value_string = reg.bits.map((e, i) => {
+                return bitstr[bitstr.length - i - 1] === '1' ? e : null;
+              }).filter((e) => e).join(',');
+            } else if (reg.frontend_type === 'enum') {
               const enumVal = reg.enum.find((e) => e.value === val);
               result[reg.register].value = val;
               result[reg.register].value_name = enumVal ? enumVal.name_en : val;
             } else {
-              result[reg.register].value = val * reg.factor ?? 1;
+              result[reg.register].value = val * (reg.factor ?? 1);
             }
           }
 
-          // this.emit(`register_${reg.register}`, result[reg.register]);
+          resolve(result);
+        } finally {
+          socket.destroy();
         }
-
-        resolve(result);
-        socket.destroy();
       });
 
       const options = {
